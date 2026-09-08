ab0db2b4296b463bfa2113a6ee988c979304e247
Rijwana <parbinrijwana8@gmail.com>
2026-09-08 19:57:28 +0530

    dsa practice

---
+++ Array_05/Twice.java
@@ -0,0 +1,17 @@
+public class Twice{
+    public static boolean containDuplicate(int nums[]){
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int array[]= {1,2,3,1};
+        System.out.print(containDuplicate(array));
+    }
+}
